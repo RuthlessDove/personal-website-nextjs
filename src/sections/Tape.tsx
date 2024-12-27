8452aefe01460d91adb7160267ef6a63720944e1
Grace <groyleuwumagbe@gmail.com>
2024-12-27 09:08:00 +0000

    Add Tape Section with marquee of key attributes

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,48 @@
+// Dependencies
+import React from "react";
+
+// Assets
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      <div className="py-16 overflow-x-clip lg:py-24">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
+          <div
+            className="flex"
+            style={{
+              maskImage:
+                "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+            }}
+          >
+            <div className="flex flex-none gap-4 py-3 animate-marquee whitespace-nowrap">
+              {words.map((word) => (
+                <div key={word} className="inline-flex gap-4 items-center">
+                  <span className="text-gray-900 uppercase font-extrabold text-small">
+                    {word}
+                  </span>
+                  <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
