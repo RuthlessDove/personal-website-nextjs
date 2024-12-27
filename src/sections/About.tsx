31bfd9264c483ce3d10af5435878427fbe35c43e
Grace <groyleuwumagbe@gmail.com>
2024-12-27 16:39:15 +0000

    Add AboutSection with personal insights, toolbox, and hobbies

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,164 @@
+// Dependencies
+import Image from "next/image";
+
+// Components
+import { SectionHeader } from "@/components/SectionHeader";
+import { TechIcon } from "@/components/TechIcon";
+import { Card } from "@/components/Card";
+import { CardHeader } from "@/components/CardHeader";
+
+// Assets
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import JavascriptIcon from "@/assets/icons/javascript.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CSSIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import mapImage from "@/assets/images/map.png";
+import smileMejomji from "@/assets/images/memoji-smile.png";
+import { ToolboxItems } from "@/components/ToolboxItems";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: CSSIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "Painting",
+    emoji: "🎨",
+    left: "5%",
+    top: "5%",
+  },
+  {
+    title: "Photography",
+    emoji: "📸",
+    left: "50%",
+    top: "5%",
+  },
+
+  {
+    title: "Music",
+    emoji: "🎵",
+    left: "70%",
+    top: "40%",
+  },
+  {
+    title: "Hiking",
+    emoji: "🥾",
+    left: "35%",
+    top: "40%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "10%",
+    top: "35%",
+  },
+  {
+    title: "Fitness",
+    emoji: "🏋️‍♂️",
+    left: "5%",
+    top: "65%",
+  },
+  {
+    title: "Reading",
+    emoji: "📚",
+    left: "45%",
+    top: "70%",
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A glimpse into my world"
+          description="Learn more about who I am, what I do, and what inspires me."
+        />
+        <div className="mt-20 flex flex-col gap-6">
+          <Card className="h-[320px]">
+            <CardHeader
+              title="My Reads"
+              description="Explore the books shaping my perspectives"
+            />
+            <div className="w-40 mx-auto mt-8 ">
+              <Image src={bookImage} alt="Book Cover" />
+            </div>
+          </Card>
+
+          <Card className="h-[320px] p-0">
+            <CardHeader
+              title="My Toolbox"
+              description="Explore the technologies and tools I use to craft exceptional digital experiences."
+              className="px-6 pt-6"
+            />
+            <div>
+              <ToolboxItems items={toolboxItems} className="mt-6" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName=" -translate-x-1/2"
+              />
+            </div>
+          </Card>
+          <Card className="h-[320px] p-0 flex flex-col">
+            <CardHeader
+              title="Beyond the Code"
+              description="Explore my interests and hobbies beyond the digital realm."
+              className="px-6 py-6"
+            />
+            <div className="relative flex-1">
+              {hobbies.map((hobby) => (
+                <div
+                  key={hobby.title}
+                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{
+                    left: hobby.left,
+                    top: hobby.top,
+                  }}
+                >
+                  <span className="font-medium text-gray-950">
+                    {hobby.title}
+                  </span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+
+          <Card>
+            <Image src={mapImage} alt="Map" />
+            <Image src={smileMejomji} alt="Smiling Memoji" />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
