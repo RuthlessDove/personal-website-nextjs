b1aac7ad5b0ac06a42498b9a1cdfabdb760c4c1d
Grace <groyleuwumagbe@gmail.com>
2024-12-28 02:49:32 +0000

    Update Footer links and adjust z-index for background elements

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,30 +4,31 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 const footerLinks = [
   {
     title: "Linkedin",
-    href: "",
+    href: "https://www.linkedin.com/in/grace-royle-uwumagbe",
   },
   {
     title: "Github",
-    href: "",
+    href: "https://github.com/RuthlessDove",
   },
 ];
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative z-0 overflow-x-clip">
       <div
-        className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-purple-200/30 -z-10"
+        className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-purple-200/30 -z-20"
         style={{
           maskImage: "radial-gradient(50% 50% at bottom center, black, transparent)"
         }}
       ></div>
-      <div className="container">
+      <div className="container z-0">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">&copy; 2024. All Rights Reserved</div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map((link) => (
               <a
-                href="#"
+                href={link.href}
+                target="_blank"
                 key={link.title}
                 className="inline-flex items-center gap-1.5"
               >
