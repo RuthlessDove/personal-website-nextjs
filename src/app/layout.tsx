4624400940319b273138ff8f11fe051b84706e63
Grace <groyleuwumagbe@gmail.com>
2024-12-28 16:20:48 +0000

    Add @vercel/analytics dependency and integrate analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const calistoga = Calistoga({
@@ -29,6 +31,7 @@ export default function RootLayout({
           "bg-gray-900 text-white antialiased font-sans"
         )}
       >
+        <Analytics/>
         {children}
       </body>
     </html>
