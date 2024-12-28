6955194bf50eaf19c1fa9e57c7b522705bcfee32
Grace <groyleuwumagbe@gmail.com>
2024-12-28 12:21:47 +0000

    Enhance TestimonialsSection: implement horizontal marquee animation for testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import felixImg from "@/assets/images/felix-profile-pic.jpeg";
 import zareenImg from "@/assets/images/zareen-profile-pic.jpeg";
 
-
 // Components
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
@@ -23,7 +22,6 @@ const testimonials = [
     text: "Grace produced impressive work during her time with us, including design suggestions that we then implemented into our main product, creating real value for the company and our users. She asks excellent questions, pushing forward team discussions. Was also wonderful to work with on a personal level, we always had interesting things to speak about! Can’t recommend her highly enough.",
     avatar: zareenImg,
   },
-  
 ];
 
 export const TestimonialsSection = () => {
@@ -44,26 +42,35 @@ export const TestimonialsSection = () => {
               "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
           }}
         >
-          <div className="flex flex-none gap-8">
-            {testimonials.map((testimonial, index) => (
-              <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8 p-6">
-                <div className="flex gap-4 items-center">
-                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center  justify-center flex-shrink-0 ">
-                    <Image
-                      src={testimonial.avatar}
-                      alt={testimonial.name}
-                      className="max-h-full rounded-full border border-gray-700"
-                    />
-                  </div>
-                  <div>
-                    <div className="font-semibold">{testimonial.name}</div>
-                    <div className="text-sm text-white/40">
-                      {testimonial.position}
+          <div className="flex flex-none gap-8 animate-move-left [animation-duration:30s] ">
+            {[...new Array(2)].fill(0).map((_, idx) => (
+              <>
+                {testimonials.map((testimonial, index) => (
+                  <Card
+                    key={testimonial.name}
+                    className="max-w-xs md:max-w-md md:p-8 p-6"
+                  >
+                    <div className="flex gap-4 items-center">
+                      <div className="size-14 bg-gray-700 inline-flex rounded-full items-center  justify-center flex-shrink-0 ">
+                        <Image
+                          src={testimonial.avatar}
+                          alt={testimonial.name}
+                          className="max-h-full rounded-full border border-gray-700"
+                        />
+                      </div>
+                      <div>
+                        <div className="font-semibold">{testimonial.name}</div>
+                        <div className="text-sm text-white/40">
+                          {testimonial.position}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
-              </Card>
+                    <p className="mt-4 md:mt-6 text-sm md:text-base">
+                      {testimonial.text}
+                    </p>
+                  </Card>
+                ))}
+              </>
             ))}
           </div>
         </div>
