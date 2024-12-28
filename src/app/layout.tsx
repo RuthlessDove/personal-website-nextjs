7b4d0c4d33f8622c8f43fc99594346bd81483efb
Grace <groyleuwumagbe@gmail.com>
2024-12-28 16:57:52 +0000

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+
+// Vercel
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -32,6 +36,7 @@ export default function RootLayout({
         )}
       >
         <Analytics/>
+        <SpeedInsights/>
         {children}
       </body>
     </html>
